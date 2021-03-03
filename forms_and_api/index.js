f600acebb5f521593bb47a9e8f3f757b0e9dc4fa
Sem-the-dev <75622197+Sem-the-dev@users.noreply.github.com>
2021-03-03 18:04:07 +0000

    Add files via upload

---
+++ forms_and_api/index.js
@@ -0,0 +1,8 @@
+let name = form.elements['greetingMessage']
+
+
+// function greetingMessage(){
+//     let greeting = document.querySelector('#greetingMessage').innerHTML = "Yoo"
+// }
+
+console.log({greetingMessage})
